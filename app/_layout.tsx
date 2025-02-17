05c865756a1aa9c81fe9f934976bea938b802807
Jay <74604447+jasonprogrammer775@users.noreply.github.com>
2025-02-17 19:04:42 +0100

    🐛 FIX:

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,15 +9,10 @@ LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
   return (
-    <>
-    <StatusBar style="light"/>
   <Stack>
-    <Stack.Screen name="(tabs)" options={{
-      headerShown: false,
-    }}/>
-  
     <Stack.Screen name="+not-found" options={{}}/>
   </Stack>
-  </>
+
+
   );
 }
